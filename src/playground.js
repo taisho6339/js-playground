112f586d571cb0248cb76251dafcbe7b6e2df7ca
Hiroki Sakamoto <taisho6339>
2016-10-13 08:52:18 +0900

    update

--- src/playground.js
+++ src/playground.js
@@ -1,9 +1,5 @@
-var set = new Set();
-set.add(23);
-set.add(33);
-set.add(23);
+var getTriangle = (base, height) => {
+    return base * height / 2;
+}
 
-const TAKE_SAMPLE = "SAMPLE";
-set.forEach(function(val) {
-    console.log(TAKE_SAMPLE);
-});
+console.log(getTriangle(2, 3));
